e74334d45417d57df0100b8d2d1d0ad409c670c3
Gregory Casamento <greg.casamento@gmail.com>
2024-08-31 00:33:02 -0400

    Initial Commit

---
+++ TestFlattenedPlist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFlattenedPlist
+//
+//  Created by Gregory John Casamento on 8/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
